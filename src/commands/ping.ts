30d15d2bdaef6f7e804e5f76066e8da2be8a2009
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-11-26 19:52:44 +0100

    created ping.ts command

---
+++ src/commands/ping.ts
@@ -0,0 +1,25 @@
+import Discord from "discord.js";
+import { manager } from "../index";
+import { colors, prefix, bot } from "../config";
+
+export = {
+    name: "ping",
+    description: "get networks stats",
+    usage: "",
+    args: false,
+    guildOnly: false,
+    execute: function (message: Discord.Message, args: Array<string>): void {
+        message.reply(new Discord.MessageEmbed({ author: { name: "Contacting my friend - Mr. Postman" }, color: colors.secondary }))
+            .then((msg) => {
+                const latency = msg.createdTimestamp - message.createdTimestamp;
+
+                const embed = new Discord.MessageEmbed()
+                    .setAuthor(`${prefix}ping`)
+                    .setColor(colors.primary)
+                    .addField("**Bot Latency**:", `${latency} ms`)
+                    .addField("**Ping**:", `${manager.client.ws.ping} ms`)
+                    .setFooter(`Mr.Postman ~ V${bot.version}`);
+                msg.edit(embed);
+            });
+    }
+};
