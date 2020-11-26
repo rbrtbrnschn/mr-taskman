49529aa631c16d4a6e69afd98399a00eac3463a4
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-11-26 19:30:18 +0100

    added clear.ts command

---
+++ src/commands/clear.ts
@@ -0,0 +1,20 @@
+import Discord from "discord.js";
+import { messages } from "../config";
+
+export = {
+    name: "clear",
+    description: "cleans channel from messages",
+    usage: "<amount of messages>",
+    args: false,
+    guildOnly: true,
+    execute: function (message: Discord.Message, args: Array<string>): void {
+        const amount = parseInt(args[0]);
+        if (args.length) {
+            if (isNaN(amount)) { message.reply(messages.args()); return; }
+        }
+        if (message.channel.type !== "dm") message.channel.bulkDelete(amount > 2 && amount <= 100 ? amount : 100)
+            .catch((err) => {
+                console.log("err", err);
+            });
+    }
+};
