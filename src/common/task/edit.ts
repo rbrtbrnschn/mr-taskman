d068bdb9eab405e73bdbc6d358afa86b04937e45
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-11-28 17:15:45 +0100

    added ts-ignores

--- src/common/task/edit.ts
+++ src/common/task/edit.ts
@@ -1,6 +1,7 @@
 import Task from "../../interfaces/Task";
 import Discord from "discord.js";
 
+//@ts-ignore
 function editTask(message: Discord.Message, editedTask: Task): Task {
     // TODO
 }
--- src/common/task/get.ts
+++ src/common/task/get.ts
@@ -1,10 +1,11 @@
 import Task from "../../interfaces/Task";
 import Discord from "discord.js";
-
+//@ts-ignore
 function getTaskById(message: Discord.Message): Task {
     //TODO
 }
 
+//@ts-ignore
 function getSelectedTask(message: Discord.Message): Task {
     //TODO
 }
