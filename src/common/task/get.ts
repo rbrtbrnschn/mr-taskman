39c2a61638ccd285c50932bb862e58b271a63e92
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-11-28 17:07:42 +0100

    overhauled readme

---
+++ src/common/task/get.ts
@@ -0,0 +1,12 @@
+import Task from "../../interfaces/Task";
+import Discord from "discord.js";
+
+function getTaskById(message: Discord.Message): Task {
+    //TODO
+}
+
+function getSelectedTask(message: Discord.Message): Task {
+    //TODO
+}
+
+export = { getTaskById, getSelectedTask };
