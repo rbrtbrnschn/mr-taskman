38f5ced4c1af47ad8ac596eca297afb2340d1e66
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-11-29 08:01:34 +0100

    added database, default user with read-only access

---
+++ src/database/schemas/task.ts
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+const taskSchema = new Schema({
+    title: String,
+    description: { type: String, default: "\u200b" },
+    participants: [],
+    deadline: Date || String,
+    taskId: String,
+    messagedId: { type: String, default: "" },
+    guildId: String, // technically not necessary
+    completed: { type: Boolean, default: false },
+    createdTimestamp: { type: Number, default: Date.now() },
+    completedTimestamp: { type: Number, default: 0 },
+    wip: { type: Boolean, default: false }
+
+});
+
+const TaskModel = mongoose.model("tasks", taskSchema);
+
+export = { TaskModel };
