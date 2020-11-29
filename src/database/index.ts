bb1d5887c1e6459f9ea24212a18d92bbf51e80d2
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-11-29 12:13:05 +0100

    console log changes in database/index.ts

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,16 +1,21 @@
 import chalk from "chalk";
 import mongoose from "mongoose";
+import { bot } from "../config";
+
 const username = process.env.MONGODB_USER || "dev";
 const password = process.env.MONGODB_PASS || "C0p5FqNEA5nV8UG7"; // provides read-only access
-const uri = `mongodb+srv://${username}:${password}@cluster0.eit8m.mongodb.net/main?retryWrites=true&w=majority`;
-
+const db = bot.isProd ? "main" : "dev";
+const uri = `mongodb+srv://${username}:${password}@cluster0.eit8m.mongodb.net/${db}?retryWrites=true&w=majority`;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((res) => console.log(`${chalk.greenBright.bold("[MONGOOSE CONNECTED]")}${chalk.reset()}`))
-    .catch((err) => console.log(`${chalk.red.bold("[MONGOOSE CONNECT]")}${chalk.reset()}`, err));
+    .then((res) => {
+        console.log(`${chalk.greenBright.bold("[MONGOOSE]")}${chalk.reset()}`);
+        console.log(`${chalk.green.bold("[DATABASE]: ")}${chalk.reset()} ${db}`);
+    })
+    .catch((err) => console.log(`${chalk.red.bold("[MONGOOSE]:")}${chalk.reset()} connection interrupted)}`, err));
 
-mongoose.connection.on("error", () => {
-    console.log(`${chalk.red.bold("[MONGOOSE ERROR]")}${chalk.reset()}`);
+mongoose.connection.on("error", (err) => {
+    console.log(`${chalk.red.bold("[MONGOOSE]:")}${chalk.reset()} errored\n ${err}`);
 });
 mongoose.connection.on("disconnected", () => {
-    console.log(`${chalk.magenta.bold("[MONGOOSE DISCONNECT]")}${chalk.reset()}`);
+    console.log(`${chalk.magenta.bold("[MONGOOSE]:]")}${chalk.reset()} lost connection`);
 });
