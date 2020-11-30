b342bb005c7e60a7f0a711c3120d48e0e287c8d9
Nick Sullivan-Friedman <rpgcubed@gmail.com>
2020-11-29 21:50:28 -0800

    style: ran linter

--- src/common/task/get.ts
+++ src/common/task/get.ts
@@ -3,12 +3,12 @@ import Discord from "discord.js";
 
 function getTaskById(message: Discord.Message): Task {
     //TODO
-    return new Task(message, 'FAKETASK');
+    return new Task(message, "FAKETASK");
 }
 
 function getSelectedTask(message: Discord.Message): Task {
     //TODO
-    return new Task(message, 'FAKETASK');
+    return new Task(message, "FAKETASK");
 }
 
 export { getTaskById, getSelectedTask };
