e21dd3bd0cb5bd420d27e5ebfad34ddd4b8be350
Nick Sullivan-Friedman <rpgcubed@gmail.com>
2020-11-29 22:23:20 -0800

    ci: dummy test command

--- src/database/index.ts
+++ src/database/index.ts
@@ -5,6 +5,7 @@ import { bot } from "../config";
 const username = process.env.MONGODB_USER || "dev";
 const password = process.env.MONGODB_PASS || "C0p5FqNEA5nV8UG7"; // provides read-only access
 const mongoHost = process.env.MONGODB_HOST || "cluster0.eit8m.mongodb.net";
+console.log(username, password, mongoHost)
 const db = bot.isProd ? "main" : "dev";
 const uri = `mongodb+srv://${username}:${password}@${mongoHost}/${db}?retryWrites=true&w=majority`;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
