91dac9fabd78f553713c0ecaba530b151732ff4e
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-11-30 06:45:45 +0100

    finalized minor changes

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,7 +4,7 @@ import { bot } from "../config";
 
 const username = process.env.MONGODB_USER || "dev";
 const password = process.env.MONGODB_PASS || "C0p5FqNEA5nV8UG7"; // provides read-only access
-const mongoURI = process.env.MONGODB_URI || 'cluster0.eit8m.mongodb.net';
+const mongoURI = process.env.MONGODB_URI || "cluster0.eit8m.mongodb.net";
 const db = bot.isProd ? "main" : "dev";
 const uri = `mongodb+srv://${username}:${password}@${mongoURI}/${db}?retryWrites=true&w=majority`;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
