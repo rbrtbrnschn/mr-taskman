f8cb56c52fe0aff3583533aa0ae901867bd95621
Moses Amiedor <atmdboss@yahoo.com>
2020-12-01 02:57:02 +0200

    onChannelDelete handler

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,13 +1,14 @@
 import chalk from "chalk";
 import mongoose from "mongoose";
-// import { bot } from '../config';
+import { bot } from "../config";
 
-// const db = bot.isProd ? "main" : "dev";
-const db = process.env.DB;
+const db = bot.isProd ? "main" : "dev";
 const username = process.env.MONGODB_USER || "dev";
 const password = process.env.MONGODB_PASS || "C0p5FqNEA5nV8UG7"; // provides read-only access
 const mongoHost = process.env.MONGODB_HOST || "cluster0.eit8m.mongodb.net";
+
 console.log(username, password, mongoHost);
+
 const uri = `mongodb+srv://${username}:${password}@${mongoHost}/${db}?retryWrites=true&w=majority`;
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
