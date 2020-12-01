a66afd0079c158d928f5b046d1a0742b9e2713f9
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-12-01 19:35:42 +0100

    removed unnecessary file

--- src/common/guild/edit.ts
+++
@@ -1,8 +0,0 @@
-import Discord from "discord.js";
-import Guild from "../../interfaces/Guild";
-
-function editGuild(message: Discord.Message, newGuild: Guild): void {
-  // todo
-}
-
-export { editGuild };
