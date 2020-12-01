299b55193e8adb82fafdd566c7369baa5e9de85e
Moses Amiedor <atmdboss@yahoo.com>
2020-12-01 09:42:18 +0200

    added channelDelete handler

---
+++ src/events/onChannelDelete.ts
@@ -0,0 +1,15 @@
+import { Channel } from "discord.js";
+import { GuildModel } from "../database/schemas";
+
+export default async function (channel: Channel) {
+  const foundGuild = await GuildModel.findOne({
+    channelIds: { $elemMatch: { $eq: channel.id } },
+  });
+  if (foundGuild) {
+    foundGuild.channelIds = foundGuild.channelIds.filter(
+      (id) => id !== channel.id
+    );
+    foundGuild.markModified("channelIds");
+    await foundGuild.save();
+  }
+}
