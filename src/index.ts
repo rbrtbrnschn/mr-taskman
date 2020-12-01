0c20cef3c017534d7cbc2ddefb5bd3c634d5546a
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-12-01 18:49:22 +0100

    removed onReaction from ./events

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import {
   onMessage,
   onGuildCreate,
   onGuildDelete,
-  onReaction,
   onChannelDelete,
 } from "./events/index";
 import propagate from "./common/propagate";
@@ -20,7 +19,6 @@ client.on("ready", onReady);
 client.on("message", onMessage);
 client.on("guildCreate", onGuildCreate);
 client.on("guildDelete", onGuildDelete);
-client.on("messageReactionAdd", onReaction);
 client.on("channelDelete", onChannelDelete);
 
 client.login();
