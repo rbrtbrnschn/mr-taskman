1e42517c0a7d280d5072b58e2c4b15e15b2c9fa5
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-12-20 11:42:11 +0100

    ✏️ Propegated Interface, Experimenting With Schema Typing

--- src/models/column.ts
+++ src/models/column.ts
@@ -4,6 +4,8 @@ import TaskInterface from "./task";
 interface ColumnBaseInterface extends mongoose.Document {
   columnId: string;
   label: string;
+  permissions: string;
+  guildId: string;
 }
 
 export interface ColumnInterface extends ColumnBaseInterface {
@@ -14,10 +16,13 @@ export interface ColumnPopulatedInterface extends ColumnBaseInterface {
   tasks: Types.Array<typeof TaskInterface>;
 }
 
-const columnSchema = new Schema({
-  columnId: String,
-  label: String,
-});
+const columnSchema = new Schema<ColumnBaseInterface>();
+// {
+// columnId: String,
+// label: String,
+// permissions: String,
+// guildId: String,
+// }
 
 const ColumnModel = mongoose.model<ColumnInterface>("columns", columnSchema);
 
