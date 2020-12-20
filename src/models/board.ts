34fd709a15303cae1a0fddd7836bba18d01e1c23
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-12-20 11:41:32 +0100

    ✏️ Propegated Interface, Experimenting With Schema Typing

--- src/models/board.ts
+++ src/models/board.ts
@@ -4,6 +4,8 @@ import ColumnInterface from "./column";
 interface BoardBaseInterface extends mongoose.Document {
   label: string;
   boardId: string;
+  ownerId: string;
+  permissions: string;
 }
 
 export interface BoardInterface extends BoardBaseInterface {
@@ -14,10 +16,11 @@ export interface BoardPopulatedInterface extends BoardBaseInterface {
   columns: Types.Array<typeof ColumnInterface>;
 }
 
-const boardSchema = new Schema({
-  label: String,
-  boardId: String,
-});
+const boardSchema = new Schema<BoardBaseInterface>();
+// {label: String,
+// boardId: String,
+// ownerId: String,
+// permissions: String,}
 
 const BoardModel = mongoose.model<BoardInterface>("boards", boardSchema);
 
