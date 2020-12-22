5e3d3bce2713e43cda86ddadde56100703062c8c
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-12-22 20:00:18 +0100

    🔨 Fixed: inconsistency

--- src/models/guild.ts
+++ src/models/guild.ts
@@ -1,22 +1,27 @@
 import TaskInterface from "./task";
+import BoardInterface from "./board";
 import mongoose, { Schema, Types } from "mongoose";
 
-interface GuildBaseInterface extends mongoose.Document {
+export interface GuildBase {
   guildId: string;
   channelIds: Array<string>;
   ownerId: string;
   nextTaskId: number;
   roles: Record<string, string>; // name : id of a Discord.Role
+  boards: unknown;
 }
+interface GuildBaseInterface extends GuildBase, mongoose.Document {}
 
 export interface GuildInterface extends GuildBaseInterface {
   tasks: Types.Array<Schema.Types.ObjectId>;
   selectedTasks: Types.Map<Types.ObjectId>;
+  boards: Types.Array<Types.ObjectId>;
 }
 
 export interface GuildPopulatedInterface extends GuildBaseInterface {
   tasks: Types.Array<typeof TaskInterface>;
   selectedTasks: Types.Map<typeof TaskInterface>;
+  boards: Types.Array<typeof BoardInterface>;
 }
 
 const guildSchema = new Schema({
@@ -43,6 +48,13 @@ const guildSchema = new Schema({
     },
     from: String,
   },
+
+  boards: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "boards",
+    },
+  ],
 });
 
 const GuildModel = mongoose.model<GuildInterface>("guilds", guildSchema);
