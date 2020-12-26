fddf131f972ec18a4e2f052ccc9350863999d5fd
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-12-26 13:18:37 +0100

    ✏️ Added services/newTask.create() | Modified: TODO

--- src/models/task.ts
+++ src/models/task.ts
@@ -4,6 +4,20 @@ import ColumnModel from "./column";
 export interface PopulatableTaskInterface {
   columns?: unknown;
 }
+/**
+ * @param {string} title - Title.
+ * @param {string} description - Description.
+ * @param {string[]} participants - List of participants.
+ * @param {Date} deadline - Deadline Date object.
+ * @param {string} taskIdentifier - Simple identifier ie. #`4072`.
+ * @param {string[]} messageIds - List of corresponding messages displaying `task`.
+ * @param {mongoose.Schema.Types.ObjectId} guildId - **ObjectId** of `guild` instance.
+ * @param {boolean} completed - Boolean, defining if `task` has been completed or not.
+ * @param {number} createdTimestamp - Unix timestamp from the moment, a `task` instance is created.
+ * @param {number} completedTimestamp - Unix timestamp from the moment a `task` instance is completed.
+ * @param {boolean} wip - Boolean, defining current status ie. "currently working on it" or "not started yet".
+ * @param {mongoose.Schema.Types.ObjectId[]} columns - List of all associated `column` instances.
+ */
 export interface TaskBase extends PopulatableTaskInterface {
   title: string;
   description?: string;
--- src/services/newTask.ts
+++ src/services/newTask.ts
@@ -1,5 +1,11 @@
-import { TaskBase } from "../models/task";
+import mongoose from "mongoose";
+import TaskModel, { TaskBase, TaskInterface } from "../models/task";
 import GenericService from "./service";
+import GuildService from "./newGuild";
+import ColumnService from "./newColumn";
+import throwError from "../utils/errors";
+import Errors from "../models/errors";
+import { ColumnInterface } from "../models/column";
 
 /**
  * Service serving as interface between all things task and the backend.
@@ -9,9 +15,70 @@ export class TaskService extends GenericService {
     super(name);
   }
   /**
-   * Creates a new Task.
+   * Instantiates a new `Task`.
    */
-  async create(taskbase: TaskBase, cb?: () => unknown): Promise<void> {
-    //
+  async create(taskbase: TaskBase): Promise<TaskInterface> {
+    const { guildId, columns } = taskbase;
+    /*Param Types
+    guildId: mongoose.Types.ObjectId
+    columns: Array<mongoose.Types.ObjectId>
+    */
+    /* Flow
+      1. validate guildId => unique and existing within global scope
+      2. validate columns[] was provided
+      3. create new task
+      4. reference task[4] in corresponding column if available.
+    */
+    try {
+      const exisitingGuildId = await GuildService.isExisitingGuildId(guildId);
+      const nextTaskIdentifier = await GuildService.getNextTaskId(guildId);
+      // [1]
+      if (!exisitingGuildId)
+        throwError(
+          `Invalid guildId: ${guildId}`,
+          Errors.malformedGuildId,
+          __dirname,
+          __filename
+        );
+      // [2]
+      if (!columns) {
+        /*
+          TODO
+          Add proper validation
+        */
+        throwError(
+          `Invalid columns: ${columns}`,
+          Errors.missingProperty,
+          __dirname,
+          __filename
+        );
+      }
+      // [3]
+      const task = new TaskModel({
+        ...taskbase,
+        taskIdentifier: nextTaskIdentifier,
+      });
+      return task.save().then(async (docs: TaskInterface) => {
+        // [4]
+        const { columns: _columns } = docs;
+        _columns.forEach((columnId) => addTaskRefToColumn(columnId, task._id));
+        return docs;
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }
+}
+
+//* Helpers
+async function addTaskRefToColumn(
+  columnId: mongoose.Schema.Types.ObjectId,
+  taskId: mongoose.Schema.Types.ObjectId
+) {
+  const column = (await ColumnService.fetch({
+    query: { key: "columnId", value: columnId },
+  })) as ColumnInterface;
+  column.tasks.push(taskId);
+  column.markModified("tasks");
+  column.save();
 }
