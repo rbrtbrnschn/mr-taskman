276239b51e2187c5af57819209b77f50f9e7649d
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-12-28 11:11:26 +0100

    ✏️ Changed: Exports

---
+++ src/services/index.ts
@@ -0,0 +1,6 @@
+import TaskService from "./newTask";
+import ColumnService from "./newColumn";
+import BoardService from "./newBoard";
+import GuildService from "./newGuild";
+
+export { TaskService, ColumnService, BoardService, GuildService };
