986bc20d50af7f9f0cd140cb2509773ecb456ac7
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-12-28 11:09:08 +0100

    🚧 - Added QueryClass, allowing for easy .transform()

--- src/models/query.ts
+++ src/models/query.ts
@@ -1,3 +1,15 @@
-export type Query<T, K extends keyof T> = { key: K; value: T[K] };
+export class QueryClass<T, K extends keyof T> {
+  key: K;
+  value: T[K];
+  constructor({ key, value } = {} as Query<T, K>) {
+    this.key = key;
+    this.value = value;
+  }
+  transform(): Record<key, unknown> {
+    return { [this.key]: this.value };
+  }
+}
 
+type key = string | number | symbol;
+export type Query<T, K extends keyof T> = { key: K; value: T[K] };
 export default Query;
